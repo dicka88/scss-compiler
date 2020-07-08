444c82d8eb89d55d98a5c4c031309ed988e3233e
dicka_ismaji <dicka_ismaji@quick.com>
2020-07-08 15:19:06 +0700

    change to ace editor, improve ui

--- app.js
+++ app.js
@@ -14,11 +14,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+/**
+ * Prototype !!!! ///**** *
+ */
 app.get("/", (req, res) => {
-	res.sendFile("./views/index.html", { root: __dirname });
+	res.sendFile("./views/scss-css.html", { root: __dirname });
+});
+
+app.get("/css-scss", (req, res) => {
+	res.sendFile("./views/css-scss.html", { root: __dirname });
 });
 
 app.use("/css", express.static("./public/css"));
+app.use("/images", express.static("./public/images"));
+app.use("/js", express.static("./public/js"));
+
 app.use("/api", indexRouter);
 app.use("*", (req, res) => res.status(404).send("not found boy"));
 
--- router/index.js
+++ router/index.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const sass = require("node-sass");
+const styleflux = require("styleflux");
+
 const routes = Router();
 
 const indexController = require("../controller/indexController");
@@ -15,7 +17,25 @@ routes.route("/scss-css").post((req, res) => {
 	try {
 		const compile = sass.renderSync({
 			data: stringSass,
-			indentedSyntax: false,
+			indentedSyntax: false, // true = sass. false = scss
+			outputStyle: "expanded",
+		});
+		res.send(compile.css.toString());
+	} catch (e) {
+		res.send(e.formatted);
+	}
+});
+
+routes.route("/sass-css").post((req, res) => {
+	const stringSass = req.body.string;
+	console.log(stringSass);
+
+	if (!stringSass) return res.status(404).send("not found");
+
+	try {
+		const compile = sass.renderSync({
+			data: stringSass,
+			indentedSyntax: true, // true = sass. false = scss
 			outputStyle: "expanded",
 		});
 		res.send(compile.css.toString());
@@ -25,7 +45,17 @@ routes.route("/scss-css").post((req, res) => {
 });
 
 routes.route("/css-scss").post((req, res) => {
-	res.send("css to scss");
+	const stringCss = req.body.string;
+	console.log(stringCss);
+
+	if (!stringCss) return res.status(404).send("not found");
+
+	try {
+		const compile = styleflux.cssToScss(stringCss);
+		res.send(compile);
+	} catch (e) {
+		res.status(400).send(e);
+	}
 });
 
 module.exports = routes;
