30d318f407ca366ecb0c83c189d1001610292705
dicka_ismaji <dicka_ismaji@quick.com>
2020-07-10 10:02:40 +0700

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,17 @@
+require("dotenv").config();
 const cssnano = require("cssnano");
 const purgecss = require("@fullhuman/postcss-purgecss");
 
+const isDev = process.env.environment === "development";
+
+const purge = () => {
+	if (isDev) return null;
+	return purgecss({
+		content: ["./views/**/*.ejs"],
+		defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+	});
+};
+
 module.exports = {
-	plugins: [
-		require("tailwindcss"),
-		require("autoprefixer"),
-		cssnano({ present: "default" }),
-		purgecss({
-			content: ["./views/**/*.ejs"],
-			defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-		}),
-	],
+	plugins: [require("tailwindcss"), require("autoprefixer"), cssnano({ present: "default" }), purge()],
 };
