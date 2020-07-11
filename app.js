04bd38a9146b6cb4ee25bc3b41948e3503cdd908
dicka_ismaji <dicka_ismaji@quick.com>
2020-07-11 11:11:41 +0700

    a

--- app.js
+++ app.js
@@ -2,31 +2,35 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
 /**
  * Load Router
  * use functional programming
  */
-const apiRouter = require("./router/index");
+const apiRouter = require("./router/api");
+const webRouter = require("./router/web");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-	res.render("pages/home");
-});
+if (process.env.environment === "development") {
+  app.set("views", path.join(__dirname, "./src/views"));
+}
+
+app.use("/", webRouter);
 
 app.use("/css", express.static("./public/css"));
 app.use("/images", express.static("./public/images"));
 app.use("/js", express.static("./public/js"));
 
 app.use("/api", apiRouter);
-app.use("*", (req, res) => res.status(404).send("not found boy"));
+app.use("*", (req, res) => res.status(404).render("pages/404"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log("server connected at " + port);
+  console.log("server connected at " + port);
 });
--- router/web.js
+++ router/web.js
@@ -1,3 +1,5 @@
+const { Router } = require("express");
+const routes = Router();
 /**
  * ________________________________________________
  *
@@ -6,3 +8,21 @@
  * _________________________________________________
  *
  */
+
+routes.get("/", (req, res) => {
+  res.render("pages/home");
+});
+
+routes.get("/css-scss", (req, res) => {
+  res.render("pages/home");
+});
+
+routes.get("/sass-css", (req, res) => {
+  res.render("pages/home");
+});
+
+routes.get("/css-sass", (req, res) => {
+  res.render("pages/home");
+});
+
+module.exports = routes;
