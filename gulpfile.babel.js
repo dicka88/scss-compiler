8968f83d5db64a033f459ac5f5c50aaee295fd2a
dicka_ismaji <dicka_ismaji@quick.com>
2020-07-13 10:44:47 +0700

    up

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,17 @@
 import { dest, src, series } from "gulp";
 import babel from "gulp-babel";
 import uglifly from "gulp-uglyfly";
-import minifyejs from "gulp-ejsmin";
+import tiny from "gulp-ejstiny";
 
 const js = () =>
   src("./src/assets/js/**/*.js")
-    .pipe(babel())
-    .pipe(uglifly())
-    .pipe(dest("./public/js"));
+  .pipe(babel())
+  .pipe(uglifly())
+  .pipe(dest("./public/js"));
 
 const ejs = () =>
   src("./src/views/**/*.ejs")
-    .pipe(minifyejs({ removeComment: true }))
-    .pipe(dest("./views/"));
+  .pipe(tiny({ removeComment: true }))
+  .pipe(dest("./views/"));
 
 export default series(ejs, js);
