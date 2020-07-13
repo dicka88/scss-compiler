d2541d8243c4602db58cfca1c6d1fe4c4d3d99f1
Dicka Ismaji <dickaismaji@gmail.com>
2020-07-13 22:10:28 +0700

    add brotli

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,19 @@ import { dest, src, series } from "gulp";
 import babel from "gulp-babel";
 import uglifly from "gulp-uglyfly";
 import tiny from "gulp-ejstiny";
+import brotli from "gulp-brotli"
 
 const js = () =>
   src("./src/assets/js/**/*.js")
   .pipe(babel())
   .pipe(uglifly())
+  .pipe(brotli.compress())
   .pipe(dest("./public/js"));
+
+const css = () =>
+  src("./public/css/**/*.css")
+    .pipe(brotli.compress())
+    .pipe(dest("./public/css/"));
 
 const ejs = () =>
   src("./src/views/**/*.ejs")
